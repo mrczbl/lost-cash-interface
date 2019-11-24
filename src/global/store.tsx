67ed2045bbe18397ee507b14b078f36e2e916370
mrczbl <marco.zabel@tuhh.de>
2019-11-24 15:38:01 +0100

    Added redux persist for token and refresh_token

--- src/global/store.tsx
+++ src/global/store.tsx
@@ -2,11 +2,26 @@ import {createStore, applyMiddleware} from 'redux';
 import {reducer} from "./reducer";
 import {rootSaga} from "./saga";
 import createSagaMiddleware from 'redux-saga';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistReducer, persistStore } from 'redux-persist';
+import { createWhitelistFilter } from 'redux-persist-transform-filter';
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['auth'],
+    transforms: [
+        createWhitelistFilter('auth', ['token', 'refresh_token']),
+    ]
+};
 
 const sagaMiddleware = createSagaMiddleware();
-export const store = createStore(
-    reducer,
+const pReducer = persistReducer(persistConfig, reducer);
+
+export const store: any = createStore(
+    pReducer,
     applyMiddleware(sagaMiddleware),
 );
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
